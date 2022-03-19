63866baad325c1dd72c524540ad7ed84b5068e1f
Anar Seferov <anarsferov@gmail.com>
2022-03-19 16:02:55 +0400

    displayed header data from API

--- src/js/displayResponse.js
+++ src/js/displayResponse.js
@@ -1,5 +1,53 @@
 import "../css/response.css"
-import { info, input, result, resultContainer, resultHeader } from "./domElements"
+import { audioElement, headerWord, info, input, partOfSpeech, phoneticListen, phoneticSound, result, resultContainer, resultHeader } from "./domElements"
+
+const showResponseUI = () => {
+    resultContainer.classList.add('show-result');
+    resultHeader.classList.add('show-result-components');
+    result.classList.add('show-result-components');
+}
+
+const displayHeaderData = (res) => {
+    partOfSpeech.innerHTML = '';
+    res.forEach(test => console.log(test));
+
+    const newResponse = res.filter(data => data.phonetics.length > 0);
+    headerWord.innerHTML = newResponse[0].word;
+
+    newResponse.forEach(data => {
+
+        data.phonetics.forEach(ph => {
+            if (Object.prototype.hasOwnProperty.call(ph, 'text')) {
+                phoneticSound.innerHTML = ph.text;
+            }
+            if (Object.prototype.hasOwnProperty.call(ph, 'audio')) {
+
+                audioElement.src = ph.audio;
+
+                phoneticListen.addEventListener('click', () => {
+                    audioElement.play();
+                    phoneticListen.classList.add("audio-listen");
+                });
+
+                audioElement.addEventListener('play', () => {
+                    setTimeout(() => {
+                        phoneticListen.classList.remove("audio-listen");
+                    }, 1000);
+                })
+            }
+        });
+
+        data.meanings.forEach(meaning => {
+
+            const regExp = new RegExp(`\\b${meaning.partOfSpeech}\\b`)
+
+            if (!(regExp.test(partOfSpeech.innerHTML))) {
+                partOfSpeech.innerHTML += `${meaning.partOfSpeech}, `;
+            }
+        });
+    });
+    partOfSpeech.innerHTML = partOfSpeech.innerHTML.slice(0, -2);
+}
 
 const displayResponse = (res) => {
     if (res.title) {
@@ -8,9 +56,8 @@ const displayResponse = (res) => {
                          `
     } else {
         info.classList.add('hide');
-        resultContainer.classList.add('show-result');
-        resultHeader.classList.add('show-result-components');
-        result.classList.add('show-result-components');
+        showResponseUI();
+        displayHeaderData(res);
     }
 }
 
--- src/js/domElements.js
+++ src/js/domElements.js
@@ -6,7 +6,23 @@ const info = document.querySelector('.info');
 const resultContainer = document.querySelector('.result-container');
 const resultHeader = document.querySelector('.result-header');
 const result = document.querySelector('.result');
-
+const headerWord = document.querySelector('.word-response');
+const partOfSpeech = document.querySelector('.part-of-speech');
+const phoneticSound = document.querySelector('.phonetic-sound');
+const audioElement = document.getElementById('audio');
+const phoneticListen = document.querySelector('.fa-volume-high');
 export {
-    input, magnifyingIcon, form, clearBtn, info, resultContainer, resultHeader, result
+    input,
+    magnifyingIcon,
+    form,
+    clearBtn,
+    info,
+    resultContainer,
+    resultHeader,
+    result,
+    headerWord,
+    partOfSpeech,
+    phoneticSound,
+    phoneticListen,
+    audioElement
 }
